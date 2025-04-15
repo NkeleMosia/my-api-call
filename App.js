0d55d7db7d859445484fe8bc999c6ba492b01d14
NkeleMosia <mosiankele39@gmail.com>
2025-04-15 13:38:00 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import DogImage from './components/DogImage'; // Adjust the path if your file structure is different
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Dog Picture App!</h1>
+      <DogImage />
+      {/* You can add other components and content here */}
+    </div>
+  );
+}
+
+export default App;
